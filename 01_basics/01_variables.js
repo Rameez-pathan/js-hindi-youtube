009ed4a767abb601536de1d340e4337bc1021d43
Rk_Pathan333 <rameezpathan222@gmail.com>
2025-12-15 07:15:59 +0000

    change in location of file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "14235"
+let accountEmail = "Rameez@google.com"
+var accountPassword = "14322"
+accountCity = "Delhi"
+
+// accountId = 2
+
+accountEmail = "hace@v.com"
+accountPassword  ="23142"
+accountCity = "Badosarai"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
